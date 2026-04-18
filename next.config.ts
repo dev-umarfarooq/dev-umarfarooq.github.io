86d207a43f6e315d17ed56dc41298b750add73e6
umar farooq <umarfarooq567800@gmail.com>
2026-04-18 12:03:03 +0500

    base path changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",       // Static HTML export
+   basePath: "/umarfarooq.github.io",
   images: {
     unoptimized: true,    // Required — GitHub Pages has no image optimizer
   },
