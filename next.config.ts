4e49fc36403c0fa5f651d6223fa20d70afd47212
umar farooq <iamomar12111@gmail.com>
2026-04-21 12:04:33 +0500

    base path removed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",       // Static HTML export
-   basePath: "/dev-umarfarooq.github.io",
+  //  basePath: "/dev-umarfarooq.github.io",
   images: {
     unoptimized: true,    // Required — GitHub Pages has no image optimizer
   },
